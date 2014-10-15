d03c07ac16a7da988bffe521373d3937e8a79c4d
gromitsun <wps_2003@hotmail.com>
2014-10-14 23:19:38 -0500

    Improved snr.py

--- cpp/python/snr/snr.py
+++ cpp/python/snr/snr.py
@@ -2,31 +2,40 @@ import numpy as np
 import snip
 
 
-def pnb_fixedwidth(ev, y, peak_center, bg_range, peak_width=100):
+def FWHM(x, noise=100, fano=0.114):
+    sigma = np.sqrt((noise / 2.3548) ** 2 + 3.58 * fano * x)
+    return 2.3548 * sigma
+
+
+def pnb(ev, y, bg, peak_center, peak_width, bg_range=None, normalize=True):
     def ev2ch(x):
         return np.abs(ev - x).argmin()
 
-    b = np.array([ev2ch(x) for x in bg_range])
     p = np.array([ev2ch(peak_center - peak_width / 2.), ev2ch(peak_center + peak_width / 2.)])
+    if bg_range is None:
+        b = p
+    else:
+        b = np.array([ev2ch(x) for x in bg_range])
 
     Nt = np.sum(y[p[0]:p[1]])
     B = 0
     nchB = 0
     for i in range(0, len(b), 2):
-        B += np.sum(y[b[i]:b[i + 1]])
+        B += np.sum(bg[b[i]:b[i + 1]])
         nchB = b[i + 1] - b[i]
-    B *= 1. * (p[1] - p[0]) / nchB
+    if normalize:
+        B *= 1. * (p[1] - p[0]) / nchB
     return Nt - B, B
+
+
+def pnb_fixedwidth(ev, y, peak_center, peak_width, bg_range):
+    return pnb(ev, y, y, peak_center, peak_width, bg_range)
 
 
 def pnb_snip(ev, y, peak_center, peak_width=100, FWHM=FWHM, offset=0., gain=10., **kwargs):
-    def ev2ch(x):
-        return np.abs(ev - x).argmin()
+    bg = snip.snip(ev, y, FWHM, offset, gain, **kwargs)
+    return pnb(ev, y, bg, peak_center, peak_width)
 
-    p = np.array([ev2ch(peak_center - peak_width / 2.), ev2ch(peak_center + peak_width / 2.)])
 
-    Nt = np.sum(y[p[0]:p[1]])
-    B = snip.snip(ev, y, FWHM, offset, gain, **kwargs)
-    B = np.sum(B[p[0]:p[1]])
-
-    return Nt - B, B
+def pnb_ascalc(ev, y, total, peak_center, peak_width):
+    return pnb(ev, total, total - y, peak_center, peak_width, normalize=False)
